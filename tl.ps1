3ae0acde7a5acfd0dd8afb260ae288c6ae008b36
DocSchneggo <docSchneggo@outlook.com>
2026-02-23 13:49:06 +0100

    Added "Only for testing" Notice

--- tl.ps1
+++ tl.ps1
@@ -1,1 +1,3 @@
+Write-Output "ONLY FOR TESTING !!!!!!!!!!!!!!!!"
 & 'C:\Users\matia\Documents\coding\Tasty Library\.venv\Scripts\python.exe' "C:\Users\matia\Documents\coding\Tasty Library\main.py"
+Write-Output "ONLY FOR TESTING !!!!!!!!!!!!!!!!"
