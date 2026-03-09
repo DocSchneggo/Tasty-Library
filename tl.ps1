2d5a0ec92e7b549c35b2fb8ecf3ef7764f13e276
DocSchneggo <119801610+DocSchneggo@users.noreply.github.com>
2026-03-09 12:13:47 +0100

    Delete tl.ps1

--- tl.ps1
+++
@@ -1,3 +0,0 @@
-Write-Output "ONLY FOR TESTING !!!!!!!!!!!!!!!!"
-C:\Users\matia\Documents\GitHub\Tasty-Library\.venv\Scripts\python.exe "C:\Users\matia\Documents\coding\Tasty Library\main.py"
-Write-Output "ONLY FOR TESTING !!!!!!!!!!!!!!!!"
